38847edbdc45a0689414c6b483f55fe7a7d48f46
pei yu ji <abc0970870@gmail.com>
2025-08-20 23:48:39 +0800

    fff

--- crawler/config.py
+++ crawler/config.py
@@ -7,6 +7,6 @@ load_dotenv()
 
 
 RABBITMQ_HOST = os.environ.get("RABBITMQ_HOST", "rabbitmq")
-RABBITMQ_PORT = os.environ.get("RABBITMQ_PORT", 15672)
+RABBITMQ_PORT = os.environ.get("RABBITMQ_PORT", 5672)
 WORKER_ACCOUNT = os.environ.get("WORKER_ACCOUNT", "worker")
 WORKER_PASSWORD = os.environ.get("WORKER_PASSWORD", "worker")
