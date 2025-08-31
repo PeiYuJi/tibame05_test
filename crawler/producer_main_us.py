02203318990212303d9a6e6707bedd9c16c0fba7
pei yu ji <abc0970870@gmail.com>
2025-08-31 16:57:07 +0800

    dasdad

--- crawler/producer_main_us.py
+++ crawler/producer_main_us.py
@@ -3,6 +3,6 @@ from crawler.tasks_etf_list_us import etf_list_us
 crawler_url = "https://tw.tradingview.com/markets/etfs/funds-usa/"
 
 print("ETF 清單")
-etf_list_us.s(crawler_url=crawler_url).apply_async(queue="etfus"
-    ).get()  # 使用 apply_async 發送任務到 RabbitMQ
-    # etf_list_us = etf_list_us(us_etf_url)  # 直接呼叫函式
+etf_list_us = etf_list_us.s(crawler_url="https://tw.tradingview.com/markets/etfs/funds-usa/")
+etf_list_us.apply_async(queue="etfus") 
+
